d27149495c9ef4d2e286d100f5da0639fff6b840
Dany Huinac <ed_xh@hotmail.com>
2017-08-25 21:29:09 -0600

    SUbiendo los nuevos cambios

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from django.utils import timezone
+
+from .models import Publicar
 
 # Create your views here.
 def listar_publicacion(request):
-        return render(request,'blog/listar_publicacion.html', {})
+        publi = Publicar.objects.filter(fecha_publica__lte=timezone.now()).order_by('fecha_publica')
+        # publi = Publicar.objects.())
+        return render(request,'blog/listar_publicacion.html', {'publi':publi})
